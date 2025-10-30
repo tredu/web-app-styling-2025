ecf93e2d9292db83eb68105f884285ea893bd2b9
AkiKurvinen <aki.kurvinen@hotmail.fi>
2025-10-30 11:45:20 +0200

    edit nextConfig

--- mui-theme/next.config.mjs
+++ mui-theme/next.config.mjs
@@ -4,6 +4,7 @@ const nextConfig = {
   output: 'export',
   basePath: '/web-app-styling-2025/mui-theme',
   assetPrefix: '/web-app-styling-2025/mui-theme',
+  images: { unoptimized: true } 
 };
 
 export default nextConfig;
