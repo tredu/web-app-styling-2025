2b5cb2f63324cc4a5bec1463ae5b7aceff15add8
AkiKurvinen <aki.kurvinen@hotmail.fi>
2025-10-30 11:18:11 +0200

    set next.js paths

--- mui-theme/next.config.mjs
+++ mui-theme/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/mui-theme',
+  assetPrefix: '/mui-theme',
 };
 
 export default nextConfig;
