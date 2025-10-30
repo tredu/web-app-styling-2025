7a071783d9e5b5ecc43e4be113217d1bd82b5297
AkiKurvinen <aki.kurvinen@hotmail.fi>
2025-10-30 11:02:27 +0200

    refine MUI theme example

--- mui-theme/src/app/page.js
+++ mui-theme/src/app/page.js
@@ -1,31 +1,43 @@
 
 import Image from "next/image";
-import styles from "./page.module.css";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import ProductCard from "../components/ProductCard";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <>
+      <Box component="header" sx={{
+        py: 2, bgcolor: 'secondary.main', color: 'secondary.contrastText',
+        borderBottom: '1px solid #ccc', padding: 1, mb: 4
+      }}>
         <Image
-          className={styles.logo}
           src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
           priority
         />
-        <div className={styles.intro}>
+        <Box>
           <Typography variant="h3" component="h1" gutterBottom>
-            To get started, edit the page.js file.
+            MUI Theme, next.js & React example
           </Typography>
           <Typography variant="body1" gutterBottom>
-            lorem ipsum dolor sit amet
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, magni.
           </Typography>
-          <Button variant="contained" color="primary">MUI Button</Button>
-        </div>
-      </main>
-    </div>
+        </Box>
+      </Box>
+      <Box component="main" sx={{ maxWidth: 1200, mx: "auto", my: 4, width: "100%" }}>
+        <Box component="section" sx={{ display: "flex", gap: 2, flexWrap: "wrap", justifyContent: "center" }}>
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </Box>
+      </Box>
+      <Box component="footer" sx={{ bgcolor: 'secondary.main', color: 'secondary.contrastText', padding: 1, paddingBottom: 4 }}>
+        <Typography variant="body1">Footer Information</Typography>
+      </Box>
+    </>
   );
 }
---
+++ mui-theme/src/components/ProductCard.js
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+export default function ProductCard() {
+    return (
+        <Card sx={{ maxWidth: 300 }}>
+        <Image
+          src="/placeholder.jpg"
+          alt="Product Image"
+          width={300}
+          height={200}
+          priority
+        />
+            <CardContent>
+                <Typography variant="body1" sx={{ mb: 2 }}>
+                    Awesome product
+                </Typography>
+                <Button variant="contained" color="primary" fullWidth>
+                    Buy Now
+                </Button>
+            </CardContent>
+        </Card>
+    );
+}
